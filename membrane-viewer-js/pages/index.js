208a8dd799b87d7f50f6abe5651b03319b07414f
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2020-11-23 00:14:35 +0100

    Revert "Routing added"
    This reverts commit ce531521d5c685ce06abf2c689f3e1fb527a7505.

--- membrane-viewer-js/pages/index.js
+++ membrane-viewer-js/pages/index.js
@@ -1,9 +1,4 @@
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import styles from './layout.module.css'
-import Link from 'next/link'
 import Head from 'next/head'
-
 import 'react-image-gallery/styles/css/image-gallery.css'
 import ImageGallery from 'react-image-gallery';
 
