2fb25c7159d6f81708ff84bb279f36bf1591c51d
Maciek Zdanowicz <m.zdanowicz@gmail.com>
2020-12-23 13:49:30 +0000

    Loading .czi files using aicsimages library

--- membrane-dispatcher/rendererMembrane/image_renderer.py
+++ membrane-dispatcher/rendererMembrane/image_renderer.py
@@ -2,12 +2,14 @@ import itertools
 from pathlib import Path
 import tempfile
 import shutil
+import os
 from tqdm import tqdm
 
 import numpy as np
 from PIL import Image
 from skimage import data, color
 import skimage.transform as transform  # rescale, resize, downscale_local_mean
+from aicsimageio import AICSImage
 
 import napari
 from napari.components import ViewerModel
@@ -38,19 +40,66 @@ class ImageRenderer(object):
             self.maskpath = None
         self.resultpath = Path(resultpath)
 
+    def load_data(self, name):
+        n = len(self.viewer.layers)
+        if name.suffix == '.czi':
+            data = AICSImage(str(name)).data
+        else:
+            self.viewer.open(str(name), layer_type='image')
+            data = self.viewer.layers[n].data
+            self.viewer.layers.pop(n)
+        d = len(data.shape)
+        print(f"load_data[shape, start]: {data.shape}")
+        # Reducing to 4 dimensions (CZYX).
+        # This is an arbitrary choice if the S (scene) or T (time) dimension
+        # is non-trivial, we just use the first frames.
+        if d == 7:
+            if data.shape[0] > 1:
+                print(f"load_data[data.shape[0]]: Equal to {data.shape[0]}.")
+            if data.shape[1] > 1:
+                print(f"load_data[data.shape[1]]: Equal to {data.shape[1]}.")
+            if data.shape[2] > 1:
+                print(f"load_data[data.shape[2]]: Equal to {data.shape[2]}.")
+            data = data[0, 0, 0, ...]
+        if d == 6:
+            if data.shape[0] > 1:
+                print(f"load_data[data.shape[0]]: Equal to {data.shape[0]}.")
+            if data.shape[1] > 1:
+                print(f"load_data[data.shape[1]]: Equal to {data.shape[1]}.")
+            data = data[0, 0, ...]
+        if d == 5:
+            if data.shape[0] > 1:
+                print(f"load_data[data.shape[0]]: Equal to {data.shape[0]}.")
+            data = data[0,...]
+        # .lsm requires transposing it is in (TZCYX) format.
+        if name.suffix == '.lsm':
+            data = np.moveaxis(data,0,1)
+        print(f"load_data[shape, end]: {data.shape}")
+        return data
+
+    def load_segm(self, name):
+        n = len(self.viewer.layers)
+        self.viewer.open(str(name))
+        data = self.viewer.layers[n].data
+        self.viewer.layers.pop(n)
+        if len(data.shape) == 4:
+            print(f"load_segm[data.shape[0]]: Equal to {data.shape[0]}.")
+            data = data[0,...]
+        print(f"load_segm[shape]: {data.shape}")
+        return data
+
     def prepareCanvas(self):
         """
         prepareCanvas
         """
         # Image
-        self.viewer.open(str(self.filepath),
-                         layer_type='image', colormap='inferno')
+        dataImage = self.load_data(self.filepath)
+        self.viewer.add_image(dataImage, colormap='turbo')
         # Mask
+        self.channels, self.z = dataImage.shape[:2]
         if self.maskpath:
-            self.viewer.open(str(self.maskpath))
-            data = np.expand_dims(self.viewer.layers[1].data,axis=0)
-            dataMask = np.stack([data, data], axis=2)
-            self.viewer.layers.pop(1)
+            data = self.load_segm(self.maskpath)
+            dataMask = np.repeat(data[None, ...], self.channels, axis=0)
             self.viewer.add_labels(dataMask, name="segmentation")
             self.viewer.layers[1].visible = False
         # self.viewer.dims.ndisplay = 2
@@ -59,13 +108,11 @@ class ImageRenderer(object):
         self.headlessRenderer.camera.zoom = 1.0
 
     def save(self, img, nameBase, folders = "", scales=[1], sizes=[]):
-        pathFolders = Path(folders)
         pathBase = self.resultpath / folders
         pathBase.mkdir(parents=True, exist_ok=True)
         for scale in scales:
             name = f"{nameBase}_x{scale}.png"
             path = pathBase / name
-            self.files.append(str(pathFolders / name))
             imgRescaled = transform.rescale(img, 1/scale, anti_aliasing=True,
                                             preserve_range=True, multichannel=True)
             io.write_png(str(path), imgRescaled.astype(np.uint8))
@@ -73,7 +120,6 @@ class ImageRenderer(object):
             x, y = size
             name = f"{nameBase}_{x}x{y}.png"
             path = pathBase / name
-            self.files.append(str(pathFolders / name))
             imgResized = transform.resize(img, size, anti_aliasing=True,
                                             preserve_range=True)
             io.write_png(str(path), imgResized.astype(np.uint8))
@@ -84,73 +130,47 @@ class ImageRenderer(object):
         """
         # The path for the results of the processing
         print(f"process: Processing {self.resultpath}..")
-        print(f"process[dims]: {self.viewer.dims.nsteps}")
 
-        dimZ = self.viewer.dims.nsteps[-4]
-        dimChannels = self.viewer.dims.nsteps[-3]
-        if len(self.viewer.dims.nsteps) == 5:
-            dimTime = self.viewer.dims.nsteps[0]
-
-        # Get all the iterable dimension (note it might be empty)
-        iterableDims = [dim for dim in self.viewer.dims.not_displayed
-                        if self.viewer.dims.nsteps[dim] > 1]
-        iterableSizes = [self.viewer.dims.nsteps[dim] for dim in self.viewer.dims.not_displayed
-                        if self.viewer.dims.nsteps[dim] > 1]
-        # Generate the ranges for the iterable dimensions
-        iterableLists = [list(range(self.viewer.dims.nsteps[dim]))
-                         for dim in iterableDims]
-        # Just a single element if no iterable dimension available
-        folders, arg = [], 0
-        if not iterableDims:
-            iterableLists = [[0]]
-        else:
-            n = len(iterableSizes)
-            arg = np.argmax(iterableSizes)
-            folders = [x for x in range(n) if x != arg]
-
-        # Iterated over all tuples of possible values in iterable dimensions
-        self.files = []
-        for elem in tqdm(list(itertools.product(*iterableLists))):
-            suffix = str(elem[arg]).zfill(2)
-            folder = "/".join([str(elem[i]).zfill(2) for i in folders])
-            resName = suffix
-            resMaskedName = (suffix + "_masked")
-            # There are some iterable dims
-            for i, dim in enumerate(iterableDims):
-                self.viewer.dims.set_point(dim, elem[i])
-
-            # Unmasked
-            img = self.headlessRenderer.render()
-            # print(f"Rendered {Path(folder) / resName}.")
-            # This renders all the sizes and scales provided in **kwargs
-            self.save(img, resName, folders=folder, **kwargs)
-            # Masked
-            if self.maskpath:
-                self.viewer.layers[1].visible = True
+        # Iterated channels and z
+        for c in tqdm(list(range(self.channels))):
+            for z in tqdm(list(range(self.z))):
+                resName = str(z).zfill(2)
+                resMaskedName = (resName + "_masked")
+                # There are some iterable dims
+                self.viewer.dims.set_point(0, c)
+                self.viewer.dims.set_point(1, z)
+                # Unmasked
                 img = self.headlessRenderer.render()
-                # print(f"Rendered {Path(folder) / resMaskedName}.")
+                # print(f"Rendered {Path(folder) / resName}.")
                 # This renders all the sizes and scales provided in **kwargs
-                self.save(img, resMaskedName, folders=folder, **kwargs)
+                self.save(img, resName, folders=str(c), **kwargs)
+                # Masked
+                if self.maskpath:
+                    self.viewer.layers[1].visible = True
+                    img = self.headlessRenderer.render()
+                    # print(f"Rendered {Path(folder) / resMaskedName}.")
+                    # This renders all the sizes and scales provided in **kwargs
+                    self.save(img, resMaskedName, folders=str(c), **kwargs)
         print("Done.")
         # Return the longest visualised dimension
         # Returning some metadata
         resultMetadata = {}
         resultMetadata['masked'] = not (self.maskpath is None)
-        resultMetadata['channels'] =  
-        if not iterableDims:
-            resultMetadata['z'] = 0
-        else:  
-            resultMetadata['z'] = np.max(iterableSizes)
+        resultMetadata['z'] = self.z
+        resultMetadata['channels'] = self.channels
         return resultMetadata
 
 
 def processImage(filepath, maskpath, resultpath, scales = [1], sizes=[]):
     tmpDir = tempfile.mkdtemp()
+    os.chmod(tmpDir, 0o775)
     renderer = ImageRenderer(filepath,maskpath,tmpDir)
     renderer.prepareCanvas()
     # Returning metadata
     metadata = renderer.process(scales=scales, sizes=sizes)
     print(f"processImage: Moving from {tmpDir} to {resultpath}.")
+    # TAKE A LOOK AT THAT
+    shutil.rmtree(resultpath, ignore_errors=True)
     shutil.move(tmpDir, resultpath)
     return metadata
 
