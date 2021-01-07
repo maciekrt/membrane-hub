278484cc58eba69ee36c7d9be780d3b8d58649f5
maciekrt <m.zdanowicz@gmail.com>
2021-01-07 20:43:34 +0000

    Better:)

--- membrane-viewer-js/run.sh
+++ membrane-viewer-js/run.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-PORT=$(cat .env.local | grep ^PORT= | cut -d'=' -f2)
+PORT=$(cat .env.local | grep ^PORT_NEXT= | cut -d'=' -f2)
 echo "Starting npm run dev on port: $PORT"
 npm run dev -- -p $PORT
+
+
