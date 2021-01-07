52e1d83eec83b6ec318ca538db5ef9051304c9bc
Maciek Zdanowicz <m.zdanowicz@gmail.com>
2021-01-07 16:04:03 +0000

    Image serving consistent with new backend metadata assumptions.

--- membrane-viewer-js/logic/serverImages.js
+++ membrane-viewer-js/logic/serverImages.js
@@ -7,19 +7,29 @@ export function processImages(email, name) {
    */
    const FOLDER = process.env.IMAGES_FOLDER;
    var fs = require('fs');
-   const metadataFile = fs.readFileSync(`${FOLDER}${name}/metadata.json`)
+   const pathDataset = `${email}/${name}`
+   const metadataFile = fs.readFileSync(`${FOLDER}${pathDataset}/metadata.json`)
    const metadata = JSON.parse(metadataFile)
    const files = [...Array(parseInt(metadata.channels))].map(
       (_, idxChannels) => {
       const flags = [false]
       if (metadata.masked == true)
          flags.push(true)
-      return flags.map((flag, idxMask) => {
+      return flags.map((flag, _) => {
          var add = flag ? "_masked" : ""
-         var arr = [...Array(parseInt(metadata.z))].map((_, idx) => ({
-            original: `/api/images/${name}/${idxChannels}/${pad(idx, 2)}${add}_x1.png`,
-            thumbnail: `/api/images/${name}/${idxChannels}/${pad(idx, 2)}${add}_100x100.png`
-         }))
+         var arr = []
+         if(metadata.dims === "2D") {
+            console.log(`logic/processImages[metadata.images]: ${metadata.images} `)
+            arr = metadata.images.map((filename, _) => ({
+               original: `/api/images/${pathDataset}/${idxChannels}/${filename}`,
+               thumbnail: `/api/images/${pathDataset}/${idxChannels}/${filename}`
+            }))
+         } else {
+            arr = metadata.images.map((filename, _) => ({
+               original: `/api/images/${pathDataset}/${idxChannels}/${filename}${add}_x1.png`,
+               thumbnail: `/api/images/${pathDataset}/${idxChannels}/${filename}${add}_100x100.png`
+            }))
+         }
          return arr
       })
    })
