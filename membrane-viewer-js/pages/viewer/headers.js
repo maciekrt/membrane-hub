255a6aa61c72617c42f337a7f3f4222acc288823
Maciek Zdanowicz <m.zdanowicz@gmail.com>
2021-01-14 17:00:55 +0000

    WIP Open Graph one of the images in public

--- membrane-viewer-js/pages/viewer/headers.js
+++ membrane-viewer-js/pages/viewer/headers.js
@@ -4,8 +4,8 @@ export default function Headers({ name, file }) {
     return (
         <Head>
             <meta property="og:title" content="Membrane Hub" />
-            <meta property="og:url" content={`https://hub.membrane.computer/api/image/${name}/${file}/`} />
-            <meta property="og:image" content={`https://hub.membrane.computer/api/image/${name}/${file}/0/0.png`} />
+            <meta property="og:url" content={`https://hub.membrane.computer/viewer/${name}/${file}/`} />
+            <meta property="og:image" content={`https://hub.membrane.computer/27348237482735.png`} />
             <meta property="og:type" content="website" />
             <meta property="og:description" content="That's some gorgeous FISH file" />
             <meta property="fb:app_id" content="180061987205320" />
