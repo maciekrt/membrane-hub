97ecc5951314873f991d4f00eb76dbb1e653c08e
maciekrt <m.zdanowicz@gmail.com>
2021-01-18 16:24:24 +0000

    WIP OG with end point testing.

--- membrane-viewer-js/pages/viewer/headers.js
+++ membrane-viewer-js/pages/viewer/headers.js
@@ -5,7 +5,9 @@ export default function Headers({ name, file, channelIdx, masked }) {
         <Head>
             <meta property="og:title" content="Membrane Hub" />
             <meta property="og:url" content={`https://hub.membrane.computer/viewer/${name}/${file}/`} />
-            <meta property="og:image" content={`https://hub.membrane.computer/api/imageOG/${name}/${file}/${channelIdx}/${masked}/`} />
+            <meta property="og:image" content={`https://hub.membrane.computer/api/imagesOG/${name}/${file}/${channelIdx}/${masked}/`} />
+            <meta property="og:image:width" content={600} />
+            <meta property="og:image:height" content={600} />
             <meta property="og:type" content="website" />
             <meta property="og:description" content="That's some gorgeous FISH file" />
             <meta property="fb:app_id" content="180061987205320" />
