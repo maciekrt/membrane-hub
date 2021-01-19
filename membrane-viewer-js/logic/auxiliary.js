0c54232f4b87d67b7ddacec1ee569a2000412c84
maciekrt <m.zdanowicz@gmail.com>
2021-01-19 23:46:42 +0000

    Translating emails to names.

--- membrane-viewer-js/logic/auxiliary.js
+++ membrane-viewer-js/logic/auxiliary.js
@@ -2,3 +2,18 @@
 export function pad(num, level) {
    return ("0".repeat(level) + num).slice(-level)
 }
+
+const namesDictionary = { 
+   "m.zdanowicz@gmail.com"          : "Maciek Zdanowicz",
+   "grzegorz.kossakowski@gmail.com" : "Grzegorz Kossakowski",
+   "a.magalska@nencki.edu.pl"       : "Adriana Magalska",
+   "oladobrosielska@gmail.com"      : "Dobra Dobrosielska",
+   "zatorskaolga@gmail.com"         : "Olga Zatorska",
+   "h.nowosielska@nencki.edu.pl"    : "Hanna Nowosielska"
+   }
+
+export function translate(email) {
+   if(email in namesDictionary)
+      return namesDictionary[email]
+   return email
+}
